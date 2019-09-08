56e514875b0f11033ca3ffb7d894b68f9b9b5837
Cornayy <cornegamer@live.nl>
2019-09-08 17:19:06 +0200

    members get added and removed from database on events

---
+++ src/events/GuildMemberAdd.ts
@@ -0,0 +1,33 @@
+import { GuildMember } from 'discord.js';
+import { Client } from '../Client';
+import Logger from '../utils/Logger';
+import { IEvent } from '../interfaces/modules/Beew';
+import { GuildModel } from '../database/Database';
+
+export default class GuildMemberAdd implements IEvent {
+    public client: Client;
+
+    constructor(client: Client) {
+        this.client = client;
+    }
+
+    async run(args: any[]): Promise<void> {
+        const member: GuildMember = args.shift();
+
+        try {
+            const { id } = member.guild;
+            const guild = await GuildModel.findOne({ id: id }).exec();
+
+            if (guild) {
+                guild.users.push({
+                    id: member.id,
+                    karma: []
+                });
+
+                guild.save();
+            }
+        } catch (e) {
+            Logger.error(e);
+        }
+    }
+}
---
+++ src/events/GuildMemberRemove.ts
@@ -0,0 +1,30 @@
+import { GuildMember } from 'discord.js';
+import { Client } from '../Client';
+import Logger from '../utils/Logger';
+import { IEvent } from '../interfaces/modules/Beew';
+import { GuildModel } from '../database/Database';
+
+export default class GuildMemberRemove implements IEvent {
+    public client: Client;
+
+    constructor(client: Client) {
+        this.client = client;
+    }
+
+    async run(args: any[]): Promise<void> {
+        const member: GuildMember = args.shift();
+
+        try {
+            const { id } = member.guild;
+            const guild = await GuildModel.findOne({ id: id }).exec();
+
+            if (guild) {
+                guild.users = guild.users.filter(user => user.id !== member.id);
+
+                guild.save();
+            }
+        } catch (e) {
+            Logger.error(e);
+        }
+    }
+}
