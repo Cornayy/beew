32ce5d3d170a8c926d4b1c4b9fa0d87c9feb713f
Cornayy <cornegamer@live.nl>
2019-09-09 12:03:41 +0200

    added database disconnect

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -23,12 +23,22 @@ export const GuildModel: Mongoose.Model<IGuild> = Mongoose.model<IGuild>('Guild'
 
 export const connect = async (): Promise<void> => {
     try {
+        Mongoose.connection.once('open', () => {
+            Logger.info('Connected to database.');
+        });
+
         await Mongoose.connect(
             `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@dev-cluster-bsmvv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
             { useNewUrlParser: true }
         );
 
-        Logger.info('Connected to database.');
+        process.on('SIGINT', () => {
+            Mongoose.connection.close(() => {
+                Logger.info('Database disconnected.');
+                // eslint-disable-next-line no-process-exit
+                process.exit(0);
+            });
+        });
     } catch (err) {
         Logger.error(err);
     }
