2e6f358be41a0db81b9b7cd23fe2e4327f80c449
Cornayy <cornegamer@live.nl>
2019-09-11 18:54:16 +0200

    random gif command added

---
+++ src/commands/RandomGif.ts
@@ -0,0 +1,46 @@
+import { Message } from 'discord.js';
+import * as fetch from 'node-fetch';
+import { Command } from '../Command';
+import { IBeewClient } from '../interfaces/modules/Beew';
+import Logger from '../utils/Logger';
+
+export default class RandomGif extends Command {
+    constructor(client: IBeewClient) {
+        super(client, {
+            name: 'randomgif',
+            description: 'Sends a random gif.',
+            category: 'Utility',
+            usage: '!randomgif <tag> (optional)',
+            cooldown: 1000,
+            requiredPermissions: ['READ_MESSAGES']
+        });
+    }
+
+    public async run(message: Message, args: any[]): Promise<boolean> {
+        try {
+            const response = await this.getResponse(args);
+            const json = await response.json();
+
+            super.respond(message.channel, json.data.url);
+
+            return true;
+        } catch (e) {
+            Logger.error(e);
+        }
+
+        return false;
+    }
+
+    async getResponse(args: any[]): Promise<any> {
+        const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_KEY}`;
+
+        if (args.length > 0) {
+            return fetch.default(
+                `https://api.giphy.com/v1/gifs/random?tag=${args.join('+')}&api_key=${
+                    process.env.GIPHY_KEY
+                }`
+            );
+        }
+        return fetch.default(endpoint);
+    }
+}
