5ff8b21e7cf7cb52d33e0f0ab68e64cd8651ef5e
Cornayy <cornegamer@live.nl>
2019-09-17 11:24:43 +0200

    added leaderboard

--- src/commands/information/Leaderboard.ts
+++ src/commands/information/Leaderboard.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, RichEmbed } from 'discord.js';
 import { Command } from '../../Command';
 import { IBeewClient } from '../../interfaces/modules/Beew';
 import Logger from '../../utils/Logger';
@@ -7,10 +7,10 @@ import { GuildModel } from '../../database/Database';
 export default class Leaderboard extends Command {
     constructor(client: IBeewClient) {
         super(client, {
-            name: 'source',
-            description: 'Sends a link to the source code.',
+            name: 'leaderboard',
+            description: 'Sends a leaderboard of users sorted by karma.',
             category: 'Information',
-            usage: '!source',
+            usage: '!leaderboard',
             cooldown: 1000,
             requiredPermissions: ['READ_MESSAGES']
         });
@@ -18,11 +18,29 @@ export default class Leaderboard extends Command {
 
     public async run(message: Message): Promise<boolean> {
         try {
-            const guild = await GuildModel.find({ id: message.guild }).exec();
+            const guild = await GuildModel.findOne({ id: message.guild }).exec();
+
+            if (guild) {
+                const { users } = guild;
+                const sortedUsers = users.sort((a, b) => (a.karma > b.karma ? 1 : -1));
+                const embed = new RichEmbed()
+                    .setTitle('User Information')
+                    .setDescription(this.conf.description)
+                    .setColor(0x00b405);
+
+                sortedUsers.forEach(async user => {
+                    const guildUser = await message.guild.fetchMember(user.id);
+                    embed.addField(guildUser.user.username, user.karma);
+                });
+
+                super.respond(message.channel, embed);
+                return true;
+            }
         } catch (e) {
             Logger.error(e);
+            return false;
         }
 
-        return true;
+        return false;
     }
 }
